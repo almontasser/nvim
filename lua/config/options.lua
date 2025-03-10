d7b2f963d9c629a67fa31edd8a9dade49f9d6a26
Mahmoud Almontasser <almontasser@outlook.com>
2025-03-10 03:47:26 +0200

    install bufferline

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,9 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
+
+vim.opt.termguicolors = true
+
+-- line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
